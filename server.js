d6a5d24021d100ee4ca13f37725844de6da32fea
CAPTAINNEGATVE <bhartisorav12@gmail.com>
2025-07-31 20:29:31 +0530

    Use dynamic PORT for Render

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const handleMerge = require("./routes/merge");
 const handleSplit = require("./routes/split");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
 
 app.use(cors());
 app.use(express.json());
